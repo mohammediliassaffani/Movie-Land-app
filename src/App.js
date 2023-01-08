dda235a384d097262afde915c83bb74fbc9c0897
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-08 21:02:54 +0100

    css-update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import { useState, useEffect } from "react";
+import { useEffect } from "react";
 import "./App.css";
 
-const App = () => {
-  const [conter, setCounter] = useState(0);
+const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=3fa61792";
 
+const App = () => {
+  const searchMovies = async (title) => {
+    const response = await fetch(`${API_URL}&s=${title}`);
+    const data = await response.json();
+    console.log(data.Search);
+  };
   useEffect(() => {
-    return () => {
-      // setCounter(0);
-    };
+    searchMovies("superman");
   }, []);
   return (
     <div className="App">
-      <button onClick={() => setCounter((prevCount) => prevCount + 1)}>
-        +
-      </button>
-      <h2>{conter}</h2>
-      <button onClick={() => setCounter((prevCount) => prevCount - 1)}>
-        -
-      </button>
+      <h1>Movie Lande</h1>
     </div>
   );
 };
