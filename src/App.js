ba45aff5e46d61b3f4deb9378d0eb9f920ab3de3
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-08 15:13:14 +0100

    fix-error1

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import "./App.css";
 const Persen = (props) => {
   <div className="Persen">
     <h1>{props.Name}</h1>
-    <h2>{props.lastnName}</h2>
+    <h2>{props.lastName}</h2>
     <h3>{props.age}</h3>
   </div>;
 };
