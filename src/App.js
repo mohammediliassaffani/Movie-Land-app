1e620203350cb6cd8e32e1125fba95f693a9285f
Mohammed Iliass Affani / <76954406+mohammediliassaffani@users.noreply.github.com>
2023-01-09 00:35:48 +0100

    show-movies

--- src/App.js
+++ src/App.js
@@ -5,17 +5,18 @@ import serachIcon from "./search.svg";
 
 const API_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=3fa61792";
 
-const movie1 = {
-  Title: "Superman/Batman: Apocalypse",
-  Year: "2010",
-  imdbID: "tt1673430",
-  Type: "movie",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BMjk3ODhmNjgtZjllOC00ZWZjLTkwYzQtNzc1Y2ZhMjY2ODE0XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
-};
+// const movie1 = {
+//   Title: "Superman/Batman: Apocalypse",
+//   Year: "2010",
+//   imdbID: "tt1673430",
+//   Type: "movie",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BMjk3ODhmNjgtZjllOC00ZWZjLTkwYzQtNzc1Y2ZhMjY2ODE0XkEyXkFqcGdeQXVyNTAyODkwOQ@@._V1_SX300.jpg",
+// };
 
 const App = () => {
   const [movies, setMovies] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
 
   const searchMovies = async (title) => {
     const response = await fetch(`${API_URL}&s=${title}`);
@@ -23,23 +24,29 @@ const App = () => {
     setMovies(data.Search);
   };
   useEffect(() => {
-    searchMovies("");
+    searchMovies(searchMovies);
   }, []);
   return (
     <div className="app">
       <h1>MovieLand</h1>
       <div className="search">
         <input
-          type="text"
           placeholder="search for movie"
-          value={"iliass"}
-          onChange={() => {}}
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <img src={serachIcon} alt="search" onClick={() => {}} />
+        <img
+          src={serachIcon}
+          alt="search"
+          onClick={() => searchMovies(searchTerm)}
+        />
       </div>
+
       {movies.length > 0 ? (
         <div className="container">
-          <MovieCard movie1={movies} />
+          {movies.map((movie) => (
+            <MovieCard movie={movie} />
+          ))}
         </div>
       ) : (
         <div className="empty">
